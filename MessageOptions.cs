f706694e15377a04e0f9bac9940cfdb235978250
tresor <tresor@fireworkx.co.za>
2021-01-06 13:02:31 +0200

    Add project files.

---
+++ MessageOptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Platform
+{
+    public class MessageOptions
+    {
+        public string CityName { get; set; } = "Kinshasa";
+        public string CountryName { get; set; } = "Congo Democratic Republic";
+    }
+}
